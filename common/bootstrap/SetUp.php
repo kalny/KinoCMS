43b508cfe20b07f085bc1580c39395c2742898c6
Anton Kalny <kalnyanton@gmail.com>
2019-03-01 11:26:04 +0200

    Adding Bootstrap class

---
+++ common/bootstrap/SetUp.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton
+ * Date: 01.03.19
+ * Time: 11:23
+ */
+
+namespace Common\bootstrap;
+
+
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+class SetUp implements BootstrapInterface
+{
+
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        // TODO: Implement bootstrap() method.
+    }
+}
